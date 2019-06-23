213896fa8191abfdef5ce7153f58eaf26ab67c9b
Zachary Zhao <zachary.m.zhao@gmail.com>
2019-06-23 16:59:21 -0700

    Designed first UI iteration w/o table

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 // Main JS file for LoLMatchDataGrabber
 
+const vm = new Vue({
+    el: '#app',
+    data: {
+        roles: ['Top', 'Jungle', 'Mid', 'Bot', 'Support'],
+        players: [],
+        visible: false,
+        link: ''
+    },
+    methods: {
+        pullData: function (event) {
+            this.visible = true
+        }
+    },
+    mounted() {
 
+    }
+});
